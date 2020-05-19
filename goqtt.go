4e7dc49cd077839967e116ee7d0cd26cbbd8a099
Andrew Schneider <aschne93@gmail.com>
2020-05-18 21:39:26 -0700

    wip - Working on client loop
    Messages are received by client, although not being parsed correctly. Loop eventually gets timed out since I'm not sending pings. Need to write a PUBLISH packet and PING packet.

--- goqtt.go
+++ goqtt.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"net"
-	"os"
 	"time"
 )
 
@@ -14,31 +13,59 @@ func main() {
 	// TODO: cli arguments
 	ip := "192.168.1.189"
 	port := "1883"
+	verbose := false       // TODO: cli argument
+	topic := "hello/world" // TODO: cli argument
+
 	conn, err := net.Dial("tcp", ip+":"+port)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-	verbose := false // TODO: cli argument
+
 	// create connection packet
 	buf := new(bytes.Buffer)
 	cpack := CreateConnectPacket()
 	cpack.Write(buf, verbose)
 
-	conn.Write(buf.Bytes())
-	fmt.Println("connected")
+	sendPacket(conn, buf.Bytes())
+	fmt.Printf("connected to %s:%s\n", ip, port)
 	time.Sleep(1 * time.Second)
 
 	// create subscription packet
-	topic := "test/topic" // TODO: cli argument
-	buf = new(bytes.Buffer)
+	buf.Reset()
 	spack := CreateSubscribePacket(topic)
 	spack.Write(buf, verbose)
-	fmt.Printf("Subscribe response %s\n", buf.String())
-	conn.Write(buf.Bytes())
-	for {
-		mustCopy(os.Stdout, conn)
+	sendPacket(conn, buf.Bytes())
+	fmt.Printf("subscribed to %s\n", topic)
+	//for {
+	//	mustCopy(os.Stdout, conn)
+	//	time.Sleep(5)
+	//	fmt.Println()
+	//}
+	subscribeLoop(conn)
+}
+
+func sendPacket(c net.Conn, packet []byte) {
+	_, err := c.Write(packet)
+	if err != nil {
+		log.Fatal(err)
 	}
+}
+
+func subscribeLoop(conn net.Conn) {
+	//r := bufio.NewReader(conn)
+	for {
+		log.Println("asdf")
+		message := make([]byte, int(26))
+		_, err := io.ReadFull(conn, message)
+		//line, err := r.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		log.Printf("%s\n", message)
+	}
+
 }
 
 func mustCopy(dst io.Writer, src io.Reader) {
