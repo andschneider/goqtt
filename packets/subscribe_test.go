9a73e868513febe5167d1bc9e618697583a71e6a
Andrew Schneider <aschne93@gmail.com>
2020-08-05 23:06:59 -0700

    Delete redundant test files
    Since CreatePacket is tested in packets_test.go it's redundant to have these tests in their own files. This cleans up the packets package a lot.
    
    Subscribe, disconnect, and publish still have individual files since they have wrappers around CreatePacket(). Might try to combine these later.

--- packets/subscribe_test.go
+++ packets/subscribe_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestSubscribePacket(t *testing.T) {
 	var buf bytes.Buffer
-	var spWrite, spRead SubscribePacket
+	var spWrite SubscribePacket
 
 	spWrite.CreateSubscribePacket(testTopic)
 	err := spWrite.Write(&buf)
@@ -16,15 +16,4 @@ func TestSubscribePacket(t *testing.T) {
 		t.Errorf("could not write %s packet: %v", spWrite.name, err)
 	}
 	fmt.Printf("write %s packet: %+v\n", spWrite.name, spWrite)
-
-	packetType, err := decodeByte(&buf)
-	if err != nil {
-		t.Errorf("could not decode type from fixed header. got %v", packetType)
-	}
-
-	err = spRead.Read(&buf)
-	if err != nil {
-		t.Errorf("could not read %s packet: %v", spRead.name, err)
-	}
-	fmt.Printf("read %s packet: %+v\n", spRead.name, spRead)
 }
